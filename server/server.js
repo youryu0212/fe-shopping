95221d25bbd0220b97ac44c7f4e5835c0f929fba
youryu0212 <youryu0212@naver.com>
2022-03-16 02:00:35 +0900

    Feat: main 태그를 위한 데이터 요청 라우팅 처리

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mainTag = require("./src/main/main-data.js");
 
 app.use(express.static(__dirname + "/../client"));
+app.use("/main", mainTag);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../client/index.html");
 });
