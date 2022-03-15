8b3159dd050640078d6688b90ac5da231d645109
youryu0212 <youryu0212@naver.com>
2022-03-15 23:21:00 +0900

    Feat: top-bar 객체 생성
    HTML Header에 들어갈 top-bar HTML 요소에 관한 객체 생성.
    객체 템플릿을 생성할 (create) 메소드와 뷰에 그려줄 (render) 메소드 구현

---
+++ client/js/header/top-bar.js
@@ -0,0 +1,37 @@
+export const TopBar = function () {};
+
+TopBar.prototype = {
+  constructor: TopBar,
+  createTemplate: function () {
+    return `
+      <nav class="top-bar-wrapper">
+        <div class="top-bar flex-row-between thousand-width-center">
+          <div class="top-bar__content-wrapper flex-row-between">
+            <div class="top-bar__content flex-row-between flex-center">
+              <div class="top-bar__text">즐겨찾기</div>
+            </div>
+            <div class="top-bar__content flex-row-between flex-center">
+              <div class="top-bar__text">입점신청</div>
+              <div class="top-bar__button"></div>
+            </div>
+          </div>
+          <div class="top-bar__content-wrapper flex-row-between">
+            <div class="top-bar__content flex-row-between flex-center">
+              <div class="top-bar__text">로그인</div>
+            </div>
+            <div class="top-bar__content flex-row-between flex-center">
+              <div class="top-bar__text">회원가입</div>
+            </div>
+            <div class="top-bar__content flex-row-between flex-center">
+              <div class="top-bar__text">고객센터</div>
+            </div>
+          </div>
+        </div>
+      </nav>
+      `;
+  },
+  render: function () {
+    const topBar = document.querySelector(".header");
+    topBar.insertAdjacentHTML("afterbegin", this.createTemplate());
+  },
+};
