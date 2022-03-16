71218219a61cef3cae48c5dc15f26fd79f935664
youryu0212 <youryu0212@naver.com>
2022-03-16 11:43:15 +0900

    Rename: 라우터 이름 변경
    각 폴더별(헤더, 메인 등) 라우팅 담당 파일을 기존 header-data.js 등에서 전부 router.js로 통일
    각 라우터에서 해당 태그에 필요한 파일 요청을 처리할 수 있도록 변경

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const mainTag = require("./src/main/main-data.js");
+const mainTag = require("./src/main/router.js");
+const headerTag = require("./src/header/router.js");
 
 app.use(express.static(__dirname + "/../client"));
 app.use("/main", mainTag);
+app.use("/header", headerTag);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/../client/index.html");
 });
