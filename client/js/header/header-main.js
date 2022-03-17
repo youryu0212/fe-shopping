c6d6d6ea73cdc4d989034ab3aace49232ec1dd81
youryu0212 <youryu0212@naver.com>
2022-03-17 16:58:47 +0900

    Feat: 검색창 메뉴 또는 검색창을 눌렀을때 펼쳐지는 영역 동적 생성 기능 추가
    검색창 또는 검색 메뉴가 클릭되면 해당하는 영역이 펼쳐질 수 있도록 동적으로 태그를 추가
    한번 더 클릭시 혹은 포커스가 벗어난 경우 해당 영역을 다시 제거하는 기능 추가

--- client/js/header/header-main.js
+++ client/js/header/header-main.js
@@ -41,9 +41,9 @@ HeaderMain.prototype = {
           alt="메인로고"
         />
         <div class="search-bar">
-          <div class="search-bar__menu flex-row-between flex-center">
-            <div class="small-text">전체</div>
-            <div class="search-bar__menu__button"></div>
+          <div class="search-bar__menu flex-row-between flex-center" tabindex="0">
+              <div class="small-text">전체</div>
+              <div class="search-bar__menu__button"></div>
           </div>
           <input
             class="search-bar__search-area"
@@ -51,8 +51,6 @@ HeaderMain.prototype = {
             placeholder="찾고 싶은 상품을 검색해 보세요!"
           />
           <div class="search-bar__search-button"></div>
-          <div class="search-bar__view-menu"></div>
-          <div class="search-bar__view-content"></div>
         </div>
         <div class="search-bar-icons">
           <div class="search-bar-icon-card icon-card">
@@ -77,6 +75,58 @@ HeaderMain.prototype = {
     const headerMain = document.querySelector(".header");
     getJsonData("/header/eventCategory").then((data) => {
       headerMain.insertAdjacentHTML("beforeend", this.createTemplate(data));
+      this.onEventRegisterSearchMenu();
     });
+  },
+  onEventRegisterSearchBar() {
+    this.onSearchMenuEvent();
+    this.onSearchViewEvent();
+  },
+  onSearchMenuEvent() {
+    const menu = document.querySelector(".search-bar__menu");
+    menu.addEventListener("click", () => this.addSearchMenu());
+    menu.addEventListener("focusout", () => this.removeSearchMenu());
+    // 에로우 펑션 아닐때는 this 바인딩이 다르게 동작. 함수가 실행할때 this가 바인딩되서, menu로 바인딩 되는데, 에로우 펑션의 경우 선언된 환경 (클래스 자체)를 가리키기 때문
+  },
+  addSearchMenu() {
+    const searchBar = document.querySelector(".search-bar");
+    if (this.removeSearchMenu()) return;
+    searchBar.insertAdjacentHTML("beforeend", this.searchViewMenu);
+    this.searchViewMenuElement = document.querySelector(".search-bar__view-menu");
+    this.runTransitionAnimation(this.searchViewMenuElement);
+  },
+  removeSearchMenu() {
+    if (this.searchViewMenuElement) {
+      this.searchViewMenuElement.remove();
+      this.searchViewMenuElement = false;
+      return true;
+    }
+    return false;
+  },
+  onSearchViewEvent() {
+    const searchArea = document.querySelector(".search-bar__search-area");
+    searchArea.addEventListener("click", () => this.addSearchView());
+    searchArea.addEventListener("focusout", () => {
+      console.log(document.activeElement);
+      this.removeSearchView();
+    });
+  },
+  addSearchView() {
+    const searchBar = document.querySelector(".search-bar");
+    if (this.removeSearchView()) return;
+    searchBar.insertAdjacentHTML("beforeend", this.searchViewContent);
+    this.searchViewContentElement = document.querySelector(".search-bar__view-content");
+    this.runTransitionAnimation(this.searchViewContentElement);
+  },
+  removeSearchView() {
+    if (this.searchViewContentElement) {
+      this.searchViewContentElement.remove();
+      this.searchViewContentElement = false;
+      return true;
+    }
+    return false;
+  },
+  runTransitionAnimation(element) {
+    setTimeout(() => element.classList.add("animation-fade"), 30);
   },
 };
