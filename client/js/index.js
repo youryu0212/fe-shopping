d6a795afc95306b3712fe85f90ae966afd40c975
youryu0212 <youryu0212@naver.com>
2022-03-18 10:42:23 +0900

    Style: 주석 제거

--- client/js/index.js
+++ client/js/index.js
@@ -2,7 +2,6 @@ import { header } from "./header/index.js";
 import { mainHome } from "./main/index.js";
 
 (() => {
-  // header가 데이터를 얻어와서 렌더하게 되면 그때부터는 promise - then 처럼 비동기 로직 순서 제어해야함
   header();
   mainHome();
 })();
